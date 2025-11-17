0736feda4736bbdbf189fc9f33d81312a332e5ad
sshaaz <sshaazshammaz@gmail.com>
2025-11-17 11:17:56 +0530

    lastfinal

--- Backend/Index.js
+++ Backend/Index.js
@@ -11,7 +11,7 @@ const PORT =process.env.PORT || 5000;
 app.use(express.json())
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://notes-frtend.onrender.com/"],
+    origin: ["http://localhost:5173", "https://notes-frtend.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
